9e8c875dce538507151fe7b28413266920f01e4b
Amy Francis <amyfrancis1997@hotmail.com>
2024-09-08 15:19:42 +0100

    various

--- config.py
+++
@@ -1,6 +0,0 @@
-# config.py
-DATA_DIR = "/Users/uw20204/CanDrivR_publishing/CanDrivR-CS/data"
-OUTPUT_DIR = "/Users/uw20204/CanDrivR_publishing/CanDrivR-CS/outputs"
-ICGC_FILE = "/Users/uw20204/CanDrivR_publishing/ICGC.tsv.gz"
-COSMIC_FILE = "/Users/uw20204/CanDrivR_publishing/COSMIC.tsv.gz"
-
