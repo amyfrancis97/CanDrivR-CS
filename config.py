64e50f57b83021b2f57d6b75a152e6552e9a27ba
Amy Francis <68270793+amyfrancis97@users.noreply.github.com>
2024-09-10 15:55:03 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
 # config.py
-DATA_DIR = "/Users/uw20204/CanDrivR_publishing/CanDrivR-CS/data"
-OUTPUT_DIR = "/Users/uw20204/CanDrivR_publishing/CanDrivR-CS/outputs"
-ICGC_FILE = "/Users/uw20204/CanDrivR_publishing/ICGC.tsv.gz"
-COSMIC_FILE = "/Users/uw20204/CanDrivR_publishing/COSMIC.tsv.gz"
-
+DATA_DIR = "/CanDrivR-CS/data"
+OUTPUT_DIR = "/CanDrivR-CS/outputs"
